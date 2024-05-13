06c679799e2e171984bfbd1b7777690fde67cac0
Yeahyun Kim <insidesy4@gmail.com>
2024-05-13 16:11:36 +0900

    [Study] spring-mvc 파일 추가

---
+++ spring-prepare/src/main/java/com/sparta/springprepare/Memo.java
@@ -0,0 +1,11 @@
+package com.sparta.springprepare;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+public class Memo {
+    private final String username;
+    private final String contents;
+
+}
