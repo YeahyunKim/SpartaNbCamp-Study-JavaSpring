ea635e0bb9ffd497b967930a9a366a70ae3dac08
Yeahyun Kim <insidesy4@gmail.com>
2024-05-14 15:41:58 +0900

    [Study] memo 프로그램 만들기

---
+++ spring-memo/src/main/java/com/sparta/springmemo/entity/Memo.java
@@ -0,0 +1,25 @@
+package com.sparta.springmemo.entity;
+
+import com.sparta.springmemo.dto.MemoRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Memo {
+    private long id;
+    private String username;
+    private String contents;
+
+    public Memo(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+
+    public void update(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+}
