eec9d17e1226df0949a13aad61e29ee4c5555e88
Yeahyun Kim <insidesy4@gmail.com>
2024-05-17 06:38:03 +0900

    [Study]  Auditing 적용하기

--- spring-memo/src/main/java/com/sparta/springmemo/dto/MemoResponseDto.java
+++ spring-memo/src/main/java/com/sparta/springmemo/dto/MemoResponseDto.java
@@ -4,21 +4,21 @@ package com.sparta.springmemo.dto;
 import com.sparta.springmemo.entity.Memo;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MemoResponseDto {
     private long id;
     private String username;
     private String contents;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public MemoResponseDto(Memo memo) {
         this.id = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
-    }
-
-    public MemoResponseDto(Long id, String username, String contents) {
-        this.id = id;
-        this.username = username;
-        this.contents = contents;
+        this.createdAt = memo.getCreatedAt();
+        this.modifiedAt = memo.getModifiedAt();
     }
 }
