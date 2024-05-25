96bf48e109228f2c93c3a5a024d3a8da5c9d243f
Yeahyun Kim <insidesy4@gmail.com>
2024-05-25 16:31:50 +0900

    [Study] Order 중간테이블 직접 생성 후 테스트

--- jpa-advance/src/main/java/com/sparta/jpaadvance/repository/OrderRepository.java
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.sparta.jpaadvance.repository;
 
+import com.sparta.jpaadvance.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
